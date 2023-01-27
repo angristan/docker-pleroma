f70dec9a44fc7556af6e249ccbacbef08d829e99
Chris Deluca <>
2023-01-27 00:39:03 +0000

    Added MetricsExporter endpoint

--- config-override.exs
+++ config-override.exs
@@ -2,3 +2,10 @@ import Config
 
 config :pleroma, :instance,
   registrations_open: false
+
+config :prometheus, Pleroma.Web.Endpoint.MetricsExporter,
+  enabled: true,
+  auth: {:basic, "prometheus", "zcEHi6KZF372cC4G"},
+  ip_whitelist: ["127.0.0.1"],
+  path: "/api/pleroma/app_metrics",
+  format: :text
