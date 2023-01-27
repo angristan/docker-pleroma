f0275031df15353b741179ad9edf4074d58d75f0
Chris Deluca <>
2023-01-27 00:47:45 +0000

    Adjusted password

--- config-override.exs
+++ config-override.exs
@@ -5,7 +5,7 @@ config :pleroma, :instance,
 
 config :prometheus, Pleroma.Web.Endpoint.MetricsExporter,
   enabled: true,
-  auth: {:basic, "prometheus", "zcEHi6KZF372cC4G"},
+  auth: {:basic, "prometheus", "${prometheus}"},
   ip_whitelist: ["127.0.0.1"],
   path: "/api/pleroma/app_metrics",
   format: :text
